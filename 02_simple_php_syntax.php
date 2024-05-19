38eb55cf3c20fcd9fba7fca29ef3644ff169b645
Anthonyyy01 <167218811+Anthonyyy01@users.noreply.github.com>
2024-05-19 20:04:05 +0800

    Add files via upload

---
+++ 02_simple_php_syntax.php
@@ -0,0 +1,40 @@
+<?php
+
+// Task 1
+$name = 'Lopera Mark Anthony A.';
+$age = 21;
+$address = 'Locib';
+$hobbies = 'Sleeping, Cooking, Watching Movies, Reading Books';
+$pet_peeve = 'Loud Noise';
+
+echo $name . '<br>';
+echo $age . '<br>';
+echo $address . '<br>';
+echo $hobbies . '<br>';
+echo $pet_peeve . '<br>';
+
+// Task 2
+
+// $hobbies = 'Sleeping, Cooking, Watching Movies, Reading Books';
+// $pet_peeve = 'Loud Noise';
+
+// Task 3
+
+define("NAME", "Lopera, Mark Anthony A.");
+define("AGE", 21);
+define("ADDRESS", "Taga Locib");
+define("HOBBIES", "Sleeping, Cooking, Watching Movies, Reading Books");
+define("PET_PEEVE", "Loud Noise");
+
+echo NAME . '<br>'; 
+echo AGE . '<br>';
+echo ADDRESS . '<br>';
+echo HOBBIES . '<br>';
+echo PET_PEEVE . '<br>';
+
+// Task 4
+var_dump(NAME, AGE, ADDRESS, HOBBIES, PET_PEEVE);
+// Task 5
+
+/*
+Loud Noise because i cannot do the things that i want to do.
